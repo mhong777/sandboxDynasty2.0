7334b70b7a3b4c958ac1bc9a95dd32ecc7f4fea0
mhong777 <mike.sj.hong@gmail.com>
2015-08-14 18:02:31 -0400

    dist

--- public/dist/application.js
+++ public/dist/application.js
@@ -55,7 +55,10 @@ angular.module('bids').config([
   '$stateProvider',
   function ($stateProvider) {
     // Bids state routing
-    $stateProvider.state('rfa', {
+    $stateProvider.state('rfa-match', {
+      url: '/rfa-match',
+      templateUrl: 'modules/bids/views/rfa-match.client.view.html'
+    }).state('rfa', {
       url: '/rfa',
       templateUrl: 'modules/bids/views/rfa.client.view.html'
     }).state('listBids', {
@@ -127,6 +130,11 @@ angular.module('bids').controller('BidsController', [
     $scope.findOne = function () {
       $scope.bid = Bids.get({ bidId: $stateParams.bidId });
     };
+  }
+]);'use strict';
+angular.module('bids').controller('RfaMatchController', [
+  '$scope',
+  function ($scope) {
   }
 ]);'use strict';
 angular.module('bids').controller('RfaController', [
@@ -704,8 +712,10 @@ angular.module('owners').controller('EditRosterController', [
       for (x = 0; x < $scope.owner.bidRoster.length; x++) {
         rfaSalary += $scope.owner.bidRoster[x].price;
       }
-      $scope.salary = Math.round(salary * 100) / 100;
-      $scope.rfaSalary = Math.round(rfaSalary * 100) / 100;
+      //$scope.salary=Math.round(salary*100)/100;
+      //$scope.rfaSalary=Math.round(rfaSalary*100)/100;
+      $scope.salary = parseFloat(salary.toFixed(2));
+      $scope.rfaSalary = parseFloat(rfaSalary.toFixed(2));
       if ($scope.owner._id == $scope.user.ownerId) {
         $scope.errMsg = false;
       } else {
@@ -713,13 +723,22 @@ angular.module('owners').controller('EditRosterController', [
       }
     };
     //MODAL
-    $scope.open = function () {
-      var modalInstance = $modal.open({
-          animation: true,
-          templateUrl: 'modules/owners/views/keeper-modal.client.view.html',
-          controller: 'ModalController',
-          size: 'lg'
-        });
+    $scope.open = function (myMode) {
+      if (myMode == 1) {
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'modules/owners/views/keeper-modal.client.view.html',
+            controller: 'ModalController',
+            size: 'lg'
+          });
+      } else {
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'modules/owners/views/rfa-modal.client.view.html',
+            controller: 'ModalController',
+            size: 'lg'
+          });
+      }
     };
   }
 ]);'use strict';
@@ -773,6 +792,109 @@ angular.module('owners').controller('MyplayersController', [
         }
       }
       socket.emit('unchoosePlayer', input);
+    };
+  }
+]);'use strict';
+// Owners controller
+angular.module('owners').controller('OwnersController', [
+  '$scope',
+  '$stateParams',
+  '$location',
+  'Authentication',
+  'Owners',
+  '$http',
+  'Users',
+  function ($scope, $stateParams, $location, Authentication, Owners, $http, Users) {
+    $scope.authentication = Authentication;
+    // Create new Owner
+    $scope.create = function () {
+      // Create new Owner object
+      var owner = new Owners({ name: this.name });
+      // Redirect after save
+      owner.$save(function (response) {
+        $location.path('owners');
+      }, function (errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+      // Clear form fields
+      this.name = '';
+    };
+    // Remove existing Owner
+    $scope.remove = function (owner) {
+      if (owner) {
+        owner.$remove();
+        for (var i in $scope.owners) {
+          if ($scope.owners[i] === owner) {
+            $scope.owners.splice(i, 1);
+          }
+        }
+        $location.path('owners');
+      } else {
+        $scope.owner.$remove(function () {
+          $location.path('owners');
+        });
+      }
+    };
+    // Update existing Owner
+    $scope.update = function () {
+      if ($scope.oldUser != $scope.owner.myUser) {
+        //de-associate the old user
+        $scope.deassociateUser;
+        var user;
+        for (var x = 0; x < $scope.allUsers.length; x++) {
+          if ($scope.allUsers[x]._id == $scope.oldUser) {
+            $scope.deassociateUser = $scope.allUsers[x];
+            $scope.deassociateUser.ownerId = null;
+            break;
+          }
+        }
+        console.log($scope.deassociateUser);
+        console.log($scope.associateUser);
+        $http.put('/users', $scope.deassociateUser).success(function (data, status) {
+          console.log('updated one user');
+          console.log(data);
+        }).then(function () {
+          //associate new user
+          $http.put('/users', $scope.associateUser).success(function (data, status) {
+            console.log('updated second user');
+            console.log(data);
+          });
+        });
+      }
+      var owner = $scope.owner;
+      $http.put('/owners/' + owner._id, owner).success(function (data, status) {
+        console.log('owner data');
+        console.log(data);
+      });
+    };
+    // Find a list of Owners
+    $scope.find = function () {
+      $scope.owners = Owners.query();
+    };
+    $scope.getUsers = function () {
+      $http.get('/allUsers').success(function (data, status) {
+        $scope.allUsers = data;
+      });
+    };
+    $scope.initializeEditOwner = function () {
+      $scope.findOne();
+      $scope.find();
+      $scope.getUsers();
+    };
+    // Find existing Owner
+    $scope.findOne = function () {
+      var ownerId = $stateParams.ownerId;
+      $http.get('/owners/' + ownerId).success(function (data, status) {
+        $scope.owner = data;
+      }).then(function () {
+        $scope.associateUser = {};
+        $scope.associateUser._id = $scope.owner.myUser;
+        $scope.oldUser = $scope.owner.myUser;
+      });
+    };
+    $scope.changeOwnerUser = function () {
+      $scope.owner.myUser = $scope.associateUser._id;
+      $scope.associateUser.ownerId = $scope.owner._id;
     };
   }
 ]);'use strict';
